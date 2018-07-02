da8b888f2e263ce018643032b3ff71e742b90222
Natarajan <reachvishwa@gmail.com>
2018-07-02 14:22:00 +0100

    changed search criteria

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -2,11 +2,11 @@ package com.develogical.app;
 
 public class QueryProcessor {
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+        if (query.toLowerCase().contains("natarajan")) {
+            return "Natarajan(26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        return "";
+        return "tests";
     }
 }
