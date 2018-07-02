d360d6a8a93ef677a35042656f1cf802430750ae
Natarajan <reachvishwa@gmail.com>
2018-07-02 15:11:32 +0100

    amended script

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -2,8 +2,8 @@ package com.develogical.app;
 
 public class QueryProcessor {
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare1")) {
-            return "Natarajan(26 April 1564 - 23 April 1616) was an " +
+        if (query.toLowerCase().contains("shakespeare")) {
+            return "Shakespeare(26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } if (query.toLowerCase().contains("tests")) return "tests ";
